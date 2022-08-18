d033a9457b146272ad49cadbf539b8f6acd9c68f
Joey Arhar <josepharhar@gmail.com>
2022-08-17 20:37:21 -0700

    deploy.sh

--- src/platform/wasm/deploy.sh
+++ src/platform/wasm/deploy.sh
@@ -6,6 +6,8 @@ git init
 echo mgba.jarhar.com > CNAME
 git add -f .
 git commit -m "github pages"
+git checkout -b pages
 git remote add origin git@github.com:josepharhar/mgba
 git push -f origin pages
+rm CNAME
 rm -rf .git
