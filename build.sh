192e75ff5a8082564b5f83e9cf8f9117477fc869
Joey Arhar <jarhar@chromium.org>
2022-08-24 15:04:08 -0600

    mac stuff

--- build.sh
+++ build.sh
@@ -3,7 +3,14 @@ set -e
 set -x
 
 rm -rf build-wasm
-docker run --rm -t -v $PWD:/home/mgba/src mgba/wasm
+#docker run --rm -t -v $PWD:/home/mgba/src mgba/wasm
+
+source $HOME/emsdk/emsdk_env.sh
+mkdir build-wasm
+cd build-wasm
+emcmake cmake ..
+make -j4 install DESTDIR=install
+cd ..
 
 cp build-wasm/wasm/mgba.js src/platform/wasm
 cp build-wasm/wasm/mgba.wasm src/platform/wasm
